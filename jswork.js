0e1f025540b986e09c5815094c40784c2821b789
atom996 <alpin.tomorri@outlook.com>
2021-11-09 18:24:56 -0500

    Major improvements done to question handling for the quiz, the base for true/false questions have been improved!

--- jswork.js
+++ jswork.js
@@ -1,10 +1,12 @@
 
-//DOMContentLoaded event listener, for loading in all of the events.
-document.addEventListener('DOMContentLoaded', () => {
-  // Set the state
-
-});
-
+const quizState = [
+  {
+    currentQuestion: 0,
+    type: "",
+    questionsRight: 0,
+    questionsWrong: 0
+  }
+];
 //Asynchronous function that retireves the question data from the fake backend.
 async function fetch_question_data(link, placement) {
   try {
@@ -21,20 +23,99 @@ async function fetch_question_data(link, placement) {
   }
 }
 
+//Initializes the quizes based on the option selected from the dropdown
 function firstTFQuestInit(){
+  document.getElementById('progressList').innerHTML = "";
   if(document.getElementById('dropSelect').value == 'opOne') {
     renderBar_function("#firstTFQuestion", 0, "https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions");
+    quizState.type = "tf";
+    quizState.currentQuestion = 0;
+    quizState.questionsWrong = 0;
+    quizState.questionsRight = 0;
+    document.getElementById("rightAnswerCount").innerHTML = "Questions Right:"
+    document.getElementById("wrongAnswerCount").innerHTML = "Questions Wrong:"
+    console.log(quizState.type);
   } else if(document.getElementById('dropSelect').value == 'opTwo') {
     renderBar_function("#firstTIQuestion", 0, "https://my-json-server.typicode.com/atom996/SPAApplication/textInputQuestions");
+    quizState.type = "ti";
+    quizState.currentQuestion = 0;
+    quizState.questionsWrong = 0;
+    quizState.questionsRight = 0;
+    document.getElementById("rightAnswerCount").innerHTML = "Questions Right:"
+    document.getElementById("wrongAnswerCount").innerHTML = "Questions Wrong:"
+    console.log(quizState.type);
+
   } else {
     alert('Please pick a quiz type!');
   }
+  var node = document.createElement('li');
+  node.appendChild(document.createTextNode('Question 1'));
+  document.getElementById('progressList').appendChild(node);
 }
 
-function firstTIQuestInit(){
-  renderBar_function("#firstTFQuestion", 0, "https://my-json-server.typicode.com/atom996/SPAApplication/textInputQuestions");
+function answerReview(){
+  if (quizState.type == "tf") {
+    alert("test");
+  } else if (quizState.type == "ti") {
+    alert("test ti");
+  }
 }
-
+//Function for testing user input if true is selected in True/False questions
+function answerTrueReview() {
+  fetch("https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions").then(response => {
+    return response.json();
+  }).then( (data) => {
+      if(data[quizState.currentQuestion].answer == "false"){
+        quizState.questionsWrong += 1;
+        quizState.currentQuestion += 1;
+        document.getElementById("wrongAnswerCount").innerHTML = `Questions Wrong: ${quizState.questionsWrong}`;
+        renderBar_function("#secondTFQuestion",quizState.currentQuestion,"https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions");
+        var node = document.createElement('li');
+        node.appendChild(document.createTextNode(`Question ${quizState.currentQuestion + 1}`));
+        document.getElementById('progressList').appendChild(node);
+      } else {
+        quizState.questionsRight += 1;
+        quizState.currentQuestion += 1;
+        document.getElementById("rightAnswerCount").innerHTML = `Questions Right: ${quizState.questionsWrong}`;
+        renderBar_function("#secondTFQuestion",quizState.currentQuestion,"https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions");
+        var node = document.createElement('li');
+        node.appendChild(document.createTextNode(`Question ${quizState.currentQuestion + 1}`));
+        document.getElementById('progressList').appendChild(node);
+      }
+  }).catch(
+    (err) => {
+      console.error(err);
+    }
+  )
+}
+//Similar function to the one above, but for the false choice.
+function answerFalseReview() {
+  fetch("https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions").then(response => {
+    return response.json();
+  }).then( (data) => {
+      if(data[quizState.currentQuestion].answer == "true"){
+        quizState.questionsWrong += 1;
+        quizState.currentQuestion += 1;
+        document.getElementById("wrongAnswerCount").innerHTML = `Questions Wrong: ${quizState.questionsWrong}`;
+        renderBar_function("#secondTFQuestion",quizState.currentQuestion,"https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions");
+        var node = document.createElement('li');
+        node.appendChild(document.createTextNode(`Question ${quizState.currentQuestion + 1}`));
+        document.getElementById('progressList').appendChild(node);
+      } else {
+        quizState.questionsRight += 1;
+        quizState.currentQuestion += 1;
+        document.getElementById("rightAnswerCount").innerHTML = `Questions Right: ${quizState.questionsRight}`;
+        renderBar_function("#secondTFQuestion",quizState.currentQuestion,"https://my-json-server.typicode.com/atom996/SPAApplication/trueFalseQuestions");
+        var node = document.createElement('li');
+        node.appendChild(document.createTextNode(`Question ${quizState.currentQuestion + 1}`));
+        document.getElementById('progressList').appendChild(node);
+      }
+  }).catch(
+    (err) => {
+      console.error(err);
+    }
+  )
+}
 //Renderer for Handlebars.js
 var renderBar_function = (view_id, dataIndex, link) => {
   //Console log to ensure that the function is running.
